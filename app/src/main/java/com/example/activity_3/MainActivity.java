6ff530a7656db780ab384c0fced8c03522e8e69f
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-03-15 02:00:30 +0700

    Activity_Menu

--- app/src/main/java/com/example/activity_3/MainActivity.java
+++ app/src/main/java/com/example/activity_3/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.activity_3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,5 +96,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat Kondisi jika yang dipilih adalah id mDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
